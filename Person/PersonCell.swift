89fbacacdc389f09906c3a26941f8c9959b676fc
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-29 22:50:40 +0300

    day 42: customised collection view

---
+++ Person/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Person
+//
+//  Created by Ekaterina Akchurina on 29.09.2020.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
--- Person/ViewController.swift
+++ Person/ViewController.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        cell.name.text = "Person"
+        return cell
+    }
+
 
 
 }
