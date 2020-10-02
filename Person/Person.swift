1799f6a9e99eca9d69c24165cfcc36f54254a415
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-10-02 19:55:42 +0300

    added codable protocol (day 49)

--- Person/Person.swift
+++ Person/Person.swift
@@ -7,19 +7,7 @@
 
 import UIKit
 
-class Person: NSObject, NSCoding {
-    
-    
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(name, forKey: "name")
-        aCoder.encode(image, forKey: "image")
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
-        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
-    }
-    
+class Person: NSObject, Codable {
     
     var name: String
     var image: String
--- Person/ViewController.swift
+++ Person/ViewController.swift
@@ -16,12 +16,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         super.viewDidLoad()
         
         // load the array
-        let defaults = UserDefaults.standard
-        if let savedPeople = defaults.object(forKey: "people") as? Data {
-            if let decodedPeople = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedPeople) as? [Person] {
-                people = decodedPeople
-            }
-        }// Do any additional setup after loading the view.
+        load()// Do any additional setup after loading the view.
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
     
@@ -111,9 +106,24 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         return paths[0]
     }
     func save() {
-        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false) {
+        
+        let jsonEncoder = JSONEncoder()
+        
+        if let savedData = try? jsonEncoder.encode(people) {
             let defaults = UserDefaults.standard
             defaults.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save people")
+        }
+    }
+    func load(){
+        let jsonDecodeer = JSONDecoder()
+        let defaults = UserDefaults.standard
+        guard let savedData = defaults.data(forKey: "people") else {fatalError()}
+        if let loadingData = try? jsonDecodeer.decode([Person].self, from: savedData) {
+            people = loadingData
+        } else {
+            print("Failed to load people")
         }
     }
 
